62ac3955a631aef99bce004d15ceb2193ba1cd94
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-06-28 19:57:28 +0000

    task 7

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,9 +2,9 @@
 for a in range(0, 9):
     for b in range(1, 10):
         if a == 8 and b == 9:
-            print("{}{}".format(a, b, end=""))
+            print("{}{}".format(a, b))
         else:
             if a < b:
-                print("{}{}, ".format(a, b))
+                print("{}{}, ".format(a, b), end="")
             else:
                 continue
