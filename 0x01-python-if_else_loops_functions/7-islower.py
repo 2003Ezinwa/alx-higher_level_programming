0b83105f5ef549c408b303180e0707f4cbe113d5
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-06-28 21:08:52 +0100

    task 8

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def islower(c):
+    for alpha in range(ord('a'), ord('z') + 1):
+        if ord(c) == alpha:
+            return True
+        else:
+            continue
+    if ord(c) != alpha:
+        return False
+
+def islower(i)
+def islower(G)
