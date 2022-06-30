951c8489f378505d8ba11f190f9d892ba2f58586
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-06-30 20:20:44 +0100

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,8 @@
-#!/usr/bin/python3
-from sys import argv
-
-def system():
-    print("{} argument".format(len(argv) - 1), end='')
-    if len(argv) == 1:
-        print("s.")
-    elif len(argv) == 2:
+#!/usr/bin/python3                          from sys import argv                                                                    def system():
+    print("{} argument".format(len(argv) - 1), end='')                                      if len(argv) == 1:
+        print("s.")                             elif len(argv) == 2:
         print(":")
     else:
-        print("s:")
-    for i in range(1, len(argv)):
+        print("s:")                             for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
-
-if __name__ == "__main__":
-    system()
+                                            if __name__ == "__main__":                      system()
