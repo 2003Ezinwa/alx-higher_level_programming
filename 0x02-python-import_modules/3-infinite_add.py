6d2c6e926fe937bf4cc185e9ceeca5313e067316
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-06-30 21:34:04 +0100

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,11 +4,11 @@ from sys import argv
 def system():
     summ = 0
     if len(argv) - 1 == 0:
-        print("{:d}".format(summ), end='')
+        print("{:d}".format(summ), end="")
     else:
         for i in range(1, len(argv)):
             summ += int(argv[i])
-        print("{:d}".format(summ), end='')
+        print("{:d}".format(summ), end="")
 
 if __name__ == "__main__":
     system()
